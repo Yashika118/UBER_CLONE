bcfcf0c76671fa4a4510893fc9d9aa6d07060447
Yashika118 <kumariyashika78@gmail.com>
2025-01-12 17:00:25 +0530

    user model is created

---
+++ backend/models/user.model.js
@@ -0,0 +1,56 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+// user schema
+
+const userSchema=new mongoose.Schema({
+   fullname:{
+        firstname:{
+            type:String,
+            required:true,
+            minlength:[3, `First name must be atleast 3 characters long.`],
+        },
+        lastname:{
+            type:String,
+            minlength:[3, `Last name must be atleast 3 characters long.`],
+        }
+   },
+   email:{
+        type:String,
+        required:true,
+        unique:true,
+        minlength:[5, `Email must be atleast 5 character long.`],
+   },
+   password:{
+        type:String,
+        required:true,
+        minlength:[5, `Password must be atleast 5 character long.`],
+        select:false,               // when we find user this particular field will not be  passed over there
+   },
+   socketId:{
+        type:String,
+   }
+})
+
+
+// user schema pe hum methods use krenge
+
+userSchema.methods.generateAuthToken=function(){
+    const token = jwt.sign({_id:this._id},process.env.JWT_SECRET);
+    return token;
+}
+
+userSchema.methods.comparePassword=async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+userSchema.statics.hashedPassword=async function(password){
+    return await bcrypt.hash(password,10);
+}
+
+
+// user model
+const userModel=mongoose.model('user',userSchema);
+
+export default userModel;
