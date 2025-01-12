fd2f8ee3ad66e33b972ae675e5f8590fda9b8273
Yashika118 <kumariyashika78@gmail.com>
2025-01-12 15:51:32 +0530

    server is running

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import http from "http";
+import app from "./app.js";
+
+
+
+const port=process.env.PORT || 3000;
+const server=http.createServer(app);
+
+server.listen(port,()=>{
+    console.log(`Server is running on port ${port}`);
+});
+
+
+// server.js is the entry point for our app therfore
+// change in package.json "main":"server.js" instead of "main":"index.js"
